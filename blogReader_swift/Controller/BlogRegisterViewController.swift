f828d66568649a1a785b18b9bf6cad47a8eaab78
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-06-28 14:12:06 +0900

    add blog register controller

---
+++ blogReader_swift/Controller/BlogRegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BlogRegisterViewController.swift
+//  blogReader_swift
+//
+//  Created by kuma on 2020/06/28.
+//  Copyright © 2020 kuma. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class BlogRegisterViewController: UIViewController {
+    
+    
+    @IBOutlet weak var urlInputTextField: UITextField!
+    
+    
+    @IBAction func tapRegister(_ sender: Any) {
+    }
+}
